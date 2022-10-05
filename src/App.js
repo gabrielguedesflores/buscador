d65a3cbf8e8dc0481cacf3cb31c814144402f46c
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-10-04 22:25:59 -0300

    ~finish him~

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
 
+import api from './services/api'
+
 function App() {
+
+  const [ input, setInput ] = useState('')
+  const [ cep, setCep ] = useState({})
+
+  async function handleSearch(){
+    
+    if(input === ''){
+      alert('Preencha o CEP')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      console.log(response.data);
+      setCep(response.data);
+      setInput('')
+
+    } catch (error) {
+      alert('Ops erro ao buscar')
+      setInput('')
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -10,21 +36,26 @@ function App() {
         <input 
           type="text"
           placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value) }
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff"/>
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 90020024</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep} </h2>
 
-        <span>Rua: Borges de Medeiros</span>
-        <span>Complemento: Apto 402</span>
-        <span>Centro Histórico</span>
-        <span>Porto Alegre - RS</span>
-      </main>
+          <span>{cep.logradouro} </span>
+          <span>Complemento: {cep.complemento} </span>
+          <span>Bairro: {cep.bairro} </span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+      
     </div>
   );
 }
