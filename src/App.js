62078ac0fb1e83903fb815218899245fae9b431c
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-10-04 22:05:13 -0300

    subindo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 90020024</h2>
+
+        <span>Rua: Borges de Medeiros</span>
+        <span>Complemento: Apto 402</span>
+        <span>Centro Histórico</span>
+        <span>Porto Alegre - RS</span>
+      </main>
     </div>
   );
 }
